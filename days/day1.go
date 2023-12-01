eac8acc51814467236a66b2c071cd608fd6a4052
Sean Barnard <sean.barnard.94@hotmail.com>
2023-12-01 16:44:14 +0000

    Day 1

--- days/day1.go
+++ days/day1.go
@@ -3,6 +3,10 @@ package days
 import (
 	"fmt"
 	"io/ioutil"
+	"sort"
+	"strconv"
+	"strings"
+	"unicode"
 )
 
 func getInput() string {
@@ -15,15 +19,79 @@ func getInput() string {
 	return string(file)
 }
 
-func day1Part1() {
-	fmt.Println("day 1 part 1")
+func day1Part1(input string) {
+	allNumbers := []int{}
+	output := 0
+	lines := strings.Split(strings.TrimRight(input, "\n"), "\n")
+	for _, line := range lines {
+		if len(line) == 0 {
+			break
+		}
+		numbers := []int{}
+		for _, char := range []rune(line) {
+			if unicode.IsNumber(char) {
+				num, err := strconv.Atoi(string(char))
+				if err != nil {
+					fmt.Println("error converting number")
+				}
+				numbers = append(numbers, num)
+			}
+		}
+		if len(numbers) > 0 {
+			both := numbers[0]*10 + numbers[len(numbers)-1]
+			allNumbers = append(allNumbers, both)
+			output = output + both
+		}
+	}
+	fmt.Println(output)
 }
 
-func day1Part2() {
-	fmt.Println("day 1 part 2")
+func day1Part2(input string) {
+	dict := make(map[string]string)
+
+	dict["one"] = "1"
+	dict["two"] = "2"
+	dict["three"] = "3"
+	dict["four"] = "4"
+	dict["five"] = "5"
+	dict["six"] = "6"
+	dict["seven"] = "7"
+	dict["eight"] = "8"
+	dict["nine"] = "9"
+
+	newInput := ""
+	for _, line := range strings.Split(strings.TrimRight(input, "\n"), "\n") {
+		var order []struct {
+			Key      string
+			Position int
+		}
+
+		for key := range dict {
+			if strings.Contains(line, key) {
+				order = append(order, struct {
+					Key      string
+					Position int
+				}{key, strings.Index(line, key)})
+			}
+		}
+		sort.Slice(order, func(i, j int) bool {
+			return order[i].Position < order[j].Position
+		})
+
+		for _, value := range order {
+			key := value.Key
+			replace := dict[key] + "" + key
+
+			line = strings.ReplaceAll(line, key, replace)
+		}
+		newInput = newInput + line + "\n"
+	}
+
+	day1Part1(newInput)
 }
 
 func Day1() {
-	day1Part1()
-	day1Part2()
+	input := getInput()
+	day1Part1(input)
+	day1Part2(input)
 }
